728425ddc2aaee8a10b69f6a569bf5e08278ee8c
Andreas Ruscheinski <andreas.ruscheinski@googlemail.com>
2018-11-18 00:45:15 +0100

    complete new installer setup

--- install.sh
+++ install.sh
@@ -5,10 +5,15 @@ BASE_DIR=`dirname ${SCRIPT}`
 echo "Make code folder"
 mkdir ~/Code ~/Code/Java ~/Code/Python ~/Code/Common-Lisp 
 
+echo "Link spacemacs and zshrc and stumpwm"
 rm ~/.spacemacs
 rm ~/.zshrc
+rm ~/.stumpwmrc
 
 ln -s -i $BASE_DIR/spacemacs ~/.spacemacs
 ln -s -i $BASE_DIR/zshrc ~/.zshrc
+ln -s -i $BASE_DIR/stumpwmrc ~./stumpwmrc
 
+echo "Extend sbclrc to lookup user-code"
 cat sbclrc >> ~/.sbclrc
+

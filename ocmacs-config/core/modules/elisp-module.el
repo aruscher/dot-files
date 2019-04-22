166398dba2526460910f467854dc102f0642a2c0
Andreas Ruscheinski <andreas.ruscheinski@googlemail.com>
2019-04-22 18:48:22 +0200

    some work on keymaps

--- ocmacs-config/core/modules/elisp-module.el
+++ ocmacs-config/core/modules/elisp-module.el
@@ -1,8 +1,24 @@
-(defun init-elisp-module ()
-  (message "Elisp module initied")
+(defun setup-show-paren ()
   (setq show-paren-style 'mixed)
   (setq show-paren-delay 0)
   (show-paren-mode))
+
+(defun setup-mode-map ()
+  (main-definer
+    "me" '(:ignore t :which-key "Evaluate")
+    "mee" '(eval-last-sexp :which-key "Evaluate Expression")
+    "meb" '(eval-buffer :which-key "Evaluate Buffer")
+    "mer" '(eval-region :which-key "Evaluate Region")
+    "mcw" '(paredit-wrap-round :which-key "Wrap Round")))
+
+
+
+(defun init-elisp-module ()
+  (message "Elisp module initied")
+  (setup-show-paren)
+  (setup-mode-map))
+
+
 
 
 (add-hook 'emacs-lisp-mode-hook #'init-elisp-module)
--- ocmacs-config/core/mymacs-keymap.el
+++ ocmacs-config/core/mymacs-keymap.el
@@ -11,38 +11,39 @@
   "" 'nil
   "a" '(:ignore t :which-key "Applications")
   "f" '(:ignore t :which-key "File")
-  "fs" 'save-buffer
-  "ff" 'helm-find-files
-  "fj" 'dired
-  "ft" 'neotree-toggle
+  "fs" '(save-buffer :which-key "Save Buffer")
+  "ff" '(helm-find-files :which-key "Find Files")
+  "fj" '(dired :which-key "Dired")
+  "ft" '(neotree-toggle :which-key "Toogle Neotree")
   "b" '(:ignore t :which-key "Buffer")
-  "bp" 'previous-buffer
-  "bn" 'next-buffer
-  "bb" 'helm-buffers-list
-  "bko" 'mymacs/kill-other-buffers
+  "bp" '(previous-buffer :which-key "Previous Buffer")
+  "bn" '(next-buffer :which-key "Next Buffer")
+  "bb" '(helm-buffers-list :which-key "Buffer List")
+  "bko" '(mymacs/kill-other-buffers :which-key "Kill Other Buffers")
   "w" '(:ignore t :which-key "Window")
-  "w-" 'split-window-below
-  "w/" 'split-window-right
-  "wh" 'evil-window-left
-  "wj" 'evil-window-down
-  "wk" 'evil-window-up
-  "wl" 'evil-window-right
+  "w-" '(split-window-below :which-key "Split Window Below")
+  "w/" '(split-window-right :which-key "Split Window Right")
+  "wh" '(evil-window-left :which-key "Window Left")
+  "wj" '(evil-window-down :which-key "Window Open")
+  "wk" '(evil-window-up :which-key "Window Up")
+  "wl" '(evil-window-right :which-key "Window Right")
   "q" '(:ignore t :which-key "Quit")
-  "qq" 'kill-emacs
-  "qr" 'mymacs/restart-emacs
-  "qd" 'mymacs/restart-emacs-debug
+  "qq" '(kill-emacs :which-key "Kill Emacs")
+  "qr" '(mymacs/restart-emacs :which-key "Restart Emacs")
+  "qd" '(mymacs/restart-emacs-debug :which-key "Restart Emacs (Debug)")
   "c" '(:ignore t :which-key "Config")
-  "cf" 'mymacs/open-config
+  "cf" '(mymacs/open-config :which-key "Open init.el")
   "cR" 'mymacs/init
   "g" '(:ignore t :which-key "Git")
-  "gs" 'magit-status
+  "gs" '(magit-status :which-key "Magit Status")
   "h" '(:ignore t :which-key "Help")
-  "hi" 'info
-  "ha" 'apropos
-  "hv" 'describe-variable
-  "hd" 'describe-mode
-  "hk" 'describe-key
-  "hf" 'describe-function)
+  "hi" '(info :which-key "Info")
+  "ha" '(apropos :which-key "Apropos")
+  "hv" '(describe-variable :which-key "Describe Variable")
+  "hd" '(describe-mode :which-key "Describe Mode")
+  "hk" '(describe-key :which-key "Describe Key")
+  "hf" '(describe-function :which-key "Describe Function")
+  )
 
 (global-set-key (kbd "M-x") 'helm-M-x)
 
@@ -63,6 +64,5 @@
 
 (evil-define-key nil custom-mode-map (kbd "j") #'widget-forward)
 (evil-define-key nil custom-mode-map (kbd "k") #'widget-backward)
-
 
 (provide 'mymacs-keymap)

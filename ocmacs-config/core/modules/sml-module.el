8ab180e0658842f00e4b38764ef4c36229c2f8be
Andreas Ruscheinski <andreas.ruscheinski@uni-rostock.de>
2019-04-23 11:38:00 +0200

    add sml smth

--- ocmacs-config/core/modules/sml-module.el
+++ ocmacs-config/core/modules/sml-module.el
@@ -1,7 +1,13 @@
+(use-package sml-mode
+  :ensure t
+  :mode ("\\.\\(sml\\|sig\\)\\'" . sml-mode) )
 
 
-
+(defun init-sml-module ()
+  (message "Init SML Module")
+  nil)
 
 
 (message "SML module loaded")
+(add-hook 'sml-mode-hook #'init-sml-module)
 (provide 'sml-module)

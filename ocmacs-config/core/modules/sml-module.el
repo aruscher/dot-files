510e0b727a01dace970b5d5b3c56c89a0630c834
Andreas Ruscheinski <andreas.ruscheinski@googlemail.com>
2019-04-23 19:23:31 +0200

    fooo

--- ocmacs-config/core/modules/sml-module.el
+++ ocmacs-config/core/modules/sml-module.el
@@ -1,11 +1,20 @@
+(setq sml-program-name "smlnj")
+
 (use-package sml-mode
   :ensure t
-  :mode ("\\.\\(sml\\|sig\\)\\'" . sml-mode) )
-
+  :mode ("\\.\\(sml\\|sig\\)\\'" . sml-mode))
 
 (defun init-sml-module ()
   (message "Init SML Module")
-  nil)
+  (setup-mode-map))
+
+(defun setup-mode-map ()
+  (main-definer
+    "me" '(:ignore t :which-key "Evaluate")
+    "mee" '(:which-key "Evaluate Expression")
+    "meb" '(sml-send-buffer :which-key "Evaluate Buffer")
+    "mer" '(sml-send-region :which-key "Evaluate Region")
+    ))
 
 
 (message "SML module loaded")

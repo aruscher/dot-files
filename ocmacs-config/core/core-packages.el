188a2fad288660b94cb7b61f1b57fdf36365b148
Andreas Ruscheinski <andreas.ruscheinski@googlemail.com>
2019-04-28 13:06:38 +0200

    cleanup packages

--- ocmacs-config/core/core-packages.el
+++ ocmacs-config/core/core-packages.el
@@ -1,120 +1,54 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-(defun core-use-package (name &rest args)
-  (progn `(use-package ,name
-	    ,@args)
-	 (message "foo")))
-
-
-(defvar core-packages
-  '((which-key
-     :config (which-key-mode))
-    (general
-     :after which-key
-     :config (general-override-mode))
-    (evil
-     :config (evil-mode))
-    (evil-magit)
-    (evil-commentary
-     :init (evil-commentary-mode))
-    (helm
-     :config (progn
-	       (setq helm-buffers-fuzzy-matching t)
-	       (helm-mode)))
-    (restart-emacs)
-    (neotree)
-    (magit)
-    (aggressive-indent
-     :hook (emacs-lisp-mode . aggressive-indent-mode))
-    (monokai-theme
-     :config (load-theme 'monokai t))
-    (rainbow-delimiters
-     :hook (prog-mode . rainbow-delimiters-mode))
-    (company
-     :config (progn
-	       (setq company-idle-delay 0)
-	       (setq company-minimum-prefix-length 3))
-     :init (global-company-mode))
-    (company-quickhelp
-     :init (add-hook 'company-mode-hook #'company-quickhelp-mode))
-    (smartparens
-     :hook (prog-mode . smartparens-mode)
-     :config (require 'smartparens-config))))
-
 
 (use-package which-key
-  :ensure t
-  :config
-  (which-key-mode))
+  :config (which-key-mode))
 
 (use-package general
-  :ensure t
   :after which-key
-  :config
-  (general-override-mode))
+  :config (general-override-mode))
 
 (use-package evil
-  :ensure t
-  :config
-  (evil-mode))
+  :config (evil-mode))
 
-(use-package evil-magit
-  :ensure t)
+(use-package evil-magit)
 
 (use-package evil-commentary
-  :ensure t
-  :init
-  (evil-commentary-mode))
+  :init (evil-commentary-mode))
 
 (use-package helm
-  :ensure t
-  :config
-  (setq helm-buffers-fuzzy-matching t)
-  (helm-mode))
+  :config (progn
+	    (setq helm-buffers-fuzzy-matching t)
+	    (helm-mode)))
 
-(use-package restart-emacs
-  :ensure t)
+(use-package restart-emacs)
 
-(use-package neotree
-  :ensure t)
+(use-package neotree)
 
-(use-package magit
-  :ensure t)
+(use-package magit)
 
 (use-package aggressive-indent
-  :ensure t
-  :hook
-  (emacs-lisp-mode . aggressive-indent-mode))
+  :hook (emacs-lisp-mode . aggressive-indent-mode))
 
 (use-package monokai-theme
-  :ensure t
-  :config
-  (load-theme 'monokai t))
+  :config (load-theme 'monokai t))
 
 (use-package rainbow-delimiters
-  :ensure t
-  :hook
-  (prog-mode . rainbow-delimiters-mode))
+  :hook (prog-mode . rainbow-delimiters-mode))
 
 (use-package company
-  :ensure t
-  :config
-  (progn
-    (setq company-idle-delay 0)
-    (setq company-minimum-prefix-length 3))
-  :init
-  (global-company-mode))
+  :config (progn
+	    (setq company-idle-delay 0)
+	    (setq company-minimum-prefix-length 3))
+  :init (global-company-mode))
 
 (use-package company-quickhelp
-  :ensure t
-  :init
-  (add-hook 'company-mode-hook #'company-quickhelp-mode))
+  :hook (company-mode-hook . company-quickhelp-mode))
+;; :init (add-hook 'company-mode-hook #'company-quickhelp-mode))
 
 (use-package smartparens
-  :ensure t
   :hook (prog-mode . smartparens-mode)
-  :config
-  (require 'smartparens-config))
+  :config (require 'smartparens-config))
 
 (provide 'core-packages)

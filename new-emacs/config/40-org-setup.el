1b41cfc52857dbb57d77390a82a73bb985996108
Andreas Ruscheinski <andreas.ruscheinski@googlemail.com>
2019-09-03 17:58:08 +0200

    prepare lisp setup

--- new-emacs/config/40-org-setup.el
+++ new-emacs/config/40-org-setup.el
@@ -5,5 +5,7 @@
 
 ;;(global-set-key "\C-cl" 'org-store-link)
 ;;(global-set-key "\C-ca" 'org-agenda)
-;;(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-cc" 'org-capture)
 ;;(global-set-key "\C-cb" 'org-switchb)
+
+

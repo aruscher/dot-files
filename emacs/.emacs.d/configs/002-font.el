06d6b201e9b5362cd772a3a18581dfac85b43ea5
Andreas Ruscheinski <andreas.ruscheinski@uni-rostock.de>
2023-09-21 14:29:10 +0200

    Fix font stuff

--- emacs/.emacs.d/configs/002-font.el
+++ emacs/.emacs.d/configs/002-font.el
@@ -1,13 +1,13 @@
-
 ;; Set default font
 (set-face-attribute 'default nil
                     :family "Fira Code Retina"
-                    :height 110
-                    :weight 'normal
-                    :width 'normal)
+                    :height 180)
+
+(set-face-attribute 'variable-pitch nil
+                    :family "Fira Code Retina"
+                    :height 180)
 
 (set-face-attribute 'fixed-pitch nil
-                    :family "Fira Code Retina"
-                    :height 110
-                    :weight 'normal
-                    :width 'normal)
+                    :family "Fira Code Retina")
+
+

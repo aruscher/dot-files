8bce57b4819fe00bd21d71214b763cbaf3e3e4fd
Andreas Ruscheinski <andreas.ruscheinski@googlemail.com>
2023-10-22 12:05:26 +0200

    fooo

--- emacs/.emacs.d/configs/002-font.el
+++ emacs/.emacs.d/configs/002-font.el
@@ -1,11 +1,11 @@
 ;; Set default font
 (set-face-attribute 'default nil
                     :family "Fira Code Retina"
-                    :height 180)
+                    :height 140)
 
 (set-face-attribute 'variable-pitch nil
                     :family "Fira Code Retina"
-                    :height 180)
+                    :height 140)
 
 (set-face-attribute 'fixed-pitch nil
                     :family "Fira Code Retina")
